cb2d755c2b02119967f6720536b19e95a87935cd
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-04-12 19:36:37 -0400

    completed exercise minus further study

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def home_page():
+    prompts = story.prompts
+    return render_template('form.html', prompts=prompts)
+
+@app.route('/story')
+def show_story():
+    text = story.generate(request.args)
+    return render_template('story.html', text=text)
